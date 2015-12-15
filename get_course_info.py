a07c316ae46184f909a610000c4d2af46676ccdb
Peter Froud <pfroud@gmail.com>
2015-12-14 23:22:08 -0800

    Getting course title and description works

--- get_course_info.py
+++ get_course_info.py
@@ -6,6 +6,8 @@ https://pisa.ucsc.edu/cs9/prd/sr9_2013/index.php.
 import requests  # html requests
 from bs4 import BeautifulSoup  # html parser
 
+# http://www.crummy.com/software/BeautifulSoup/bs4/doc/
+
 # Taken from chrome inspector. Go to Network tab before clicking Search on class search form.
 # Click Search, then click on index.php in the Network tab.
 # In the Headers tab, scroll down to Form Data. This is the HTML POST data.
@@ -28,11 +30,11 @@ payload = {
     'binds[:times]': '',
     'binds[:acad_career]': ''}
 
-# do the request. you get a page of class search results.
-r = requests.post('https://pisa.ucsc.edu/class_search/', data=payload)
+# do the request for the search results page
+request_result = requests.post('https://pisa.ucsc.edu/class_search/', data=payload)
 
 # feed search results page to parser
-soup = BeautifulSoup(r.text, 'html.parser')
+soup = BeautifulSoup(request_result.text, 'html.parser')
 
 # Grotesque element selection. we want class_data for a link to a class.
 # Get the <a> element which is the Class Title link in the first row of results.
@@ -43,13 +45,15 @@ encoded_course = soup.select('tbody td:nth-of-type(3) a')[0]['href'].split('=')[
 result_url = 'https://pisa.ucsc.edu/class_search/index.php?action=detail&class_data=' + encoded_course
 print(result_url)
 
+# loads the course info page
+request_result = requests.get(result_url)
+
 # feed course info page to parser
-soup = BeautifulSoup(result_url, 'html.parser')
+soup = BeautifulSoup(request_result.text, 'html.parser')
 
-# this doesn't work yet
-# course_name = soup.select("table.PALEVEL0SECONDARY tr:nth-of-type(2) td")
-course_name = soup.select("table.PALEVEL0SECONDARY")
-print("course_name is", course_name)
+# this does now work
+course_name = soup.select("table.PALEVEL0SECONDARY tr:nth-of-type(2) td")[0].text
+print("course_name is \"" + course_name + "\"")
 
-course_description = soup.select("table.detail_table")
-print("course_description is", course_description)
+course_description = soup.select("table.detail_table")[1].select("td")[0].text
+print("course_description is \"" + course_description + "\"")
