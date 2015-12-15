7a1a912ee36112c4f9a1784bc1d4f4f44810d0be
Peter Froud <pfroud@gmail.com>
2015-12-14 23:10:47 -0800

    Added comments

--- get_course_info.py
+++ get_course_info.py
@@ -2,11 +2,13 @@
 Given a string of a department and course number, pulls information about the course from
 https://pisa.ucsc.edu/cs9/prd/sr9_2013/index.php.
 """
-from pprint import pprint
 
-import requests
-from bs4 import BeautifulSoup
+import requests  # html requests
+from bs4 import BeautifulSoup  # html parser
 
+# Taken from chrome inspector. Go to Network tab before clicking Search on class search form.
+# Click Search, then click on index.php in the Network tab.
+# In the Headers tab, scroll down to Form Data. This is the HTML POST data.
 payload = {
     'action': 'results',
     'binds[:term]': '2160',
@@ -26,16 +28,28 @@ payload = {
     'binds[:times]': '',
     'binds[:acad_career]': ''}
 
+# do the request. you get a page of class search results.
 r = requests.post('https://pisa.ucsc.edu/class_search/', data=payload)
 
+# feed search results page to parser
 soup = BeautifulSoup(r.text, 'html.parser')
+
+# Grotesque element selection. we want class_data for a link to a class.
+# Get the <a> element which is the Class Title link in the first row of results.
+# Get the only element in the resulting array. Get the href string, then split by '=' and get second element.
 encoded_course = soup.select('tbody td:nth-of-type(3) a')[0]['href'].split('=')[2]
+
+# stick class_data to the end of this thing. you'll get a page with course information.
 result_url = 'https://pisa.ucsc.edu/class_search/index.php?action=detail&class_data=' + encoded_course
 print(result_url)
 
+# feed course info page to parser
 soup = BeautifulSoup(result_url, 'html.parser')
-course_name = soup.select("table.PALEVEL0SECONDARY tr:nth-of-type(2) td")
+
+# this doesn't work yet
+# course_name = soup.select("table.PALEVEL0SECONDARY tr:nth-of-type(2) td")
+course_name = soup.select("table.PALEVEL0SECONDARY")
 print("course_name is", course_name)
+
 course_description = soup.select("table.detail_table")
 print("course_description is", course_description)
-
